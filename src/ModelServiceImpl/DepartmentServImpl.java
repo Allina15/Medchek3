97d2e07b16c06cc87e950118190237f0e15e949d
JandarJavaSenior <xp3335@email.com>
2023-10-04 11:34:13 +0600

    instal commit

--- src/ModelServiceImpl/DepartmentServImpl.java
+++ src/ModelServiceImpl/DepartmentServImpl.java
@@ -2,7 +2,10 @@ package ModelServiceImpl;
 
 import DaoService.DepartmentDao;
 import Database.Dao;
+import Model.Department;
 import ModelService.DepartmentService;
+
+import java.util.List;
 
 public class DepartmentServImpl implements DepartmentService {
     private Dao dao;
@@ -11,5 +14,30 @@ public class DepartmentServImpl implements DepartmentService {
     public DepartmentServImpl(Dao dao, DepartmentDao departmentDao) {
         this.dao = dao;
         this.departmentDao = departmentDao;
+    }
+
+    @Override
+    public String addDepartmentToHospital(Long id, Department department) {
+        return departmentDao.addDepartmentToHospital(id,department);
+    }
+
+    @Override
+    public List<Department> getAllDepartmentByHospital(Long id) {
+        return departmentDao.getAllDepartmentByHospital(id);
+    }
+
+    @Override
+    public Department findDepartmentByName(String name) {
+        return departmentDao.findDepartmentByName(name);
+    }
+
+    @Override
+    public void deleteDepartmentById(Long id) {
+        departmentDao.deleteDepartmentById(id);
+    }
+
+    @Override
+    public String updateDepartmentById(Long id, Department department) {
+        return departmentDao.updateDepartmentById(id,department);
     }
 }
